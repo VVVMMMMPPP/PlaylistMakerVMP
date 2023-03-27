9138587dda0287b3e02f61326ab02ea34313ba35
VVVMMMMPPP <Vladsmog18@mailru>
2023-03-27 21:24:08 +0300

    Added interacted buttons

--- app/src/main/java/com/example/playlistmakervmp/MainActivity.kt
+++ app/src/main/java/com/example/playlistmakervmp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.playlistmakervmp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Тут будет поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+
+        val buttonMedia=findViewById<Button>(R.id.media)
+        buttonMedia.setOnClickListener { Toast.makeText(this,"Тут будет Медиа!", Toast.LENGTH_SHORT).show() }
+
+        val buttonSettings=findViewById<Button>(R.id.settings)
+        buttonSettings.setOnClickListener { Toast.makeText(this,"Тут будут настройки!", Toast.LENGTH_SHORT).show() }
     }
 }
