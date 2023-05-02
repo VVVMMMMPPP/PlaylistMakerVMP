9bacbcad229e0618e4d328f19539293f82ea9918
VVVMMMMPPP <Vladsmog18@mailru>
2023-05-02 12:45:59 +0300

    Sprint9try#2

--- app/src/main/java/com/example/playlistmakervmp/MainActivity.kt
+++ app/src/main/java/com/example/playlistmakervmp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import android.widget.Button
 
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         val buttonSearch = findViewById<Button>(R.id.search)
         val buttonMedia=findViewById<Button>(R.id.media)
--- app/src/main/java/com/example/playlistmakervmp/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmakervmp/SettingsActivity.kt
@@ -14,31 +14,34 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.settings_screen)
 
         val backButton = findViewById<ImageView>(R.id.back)
-        backButton.setOnClickListener { val displayIntent= Intent(this,MainActivity::class.java)
-            startActivity(displayIntent)  }
+        backButton.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
 
-        val shareApp= findViewById<TextView>(R.id.shareApp)
+        val shareApp = findViewById<TextView>(R.id.shareApp)
         shareApp.setOnClickListener {
-            val sendIntent= Intent(Intent.ACTION_SEND)
+            val sendIntent = Intent(Intent.ACTION_SEND)
             sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.linkCourse))
             sendIntent.type = "text/plain"
             startActivity(Intent.createChooser(sendIntent, "Share"))
         }
         val supportButton = findViewById<TextView>(R.id.supportButton)
         supportButton.setOnClickListener {
-            val shareIntent= Intent(Intent.ACTION_SENDTO)
-            shareIntent.data = Uri.parse("mailto:")
-            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(R.string.myEMail))
-            shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.emailSubject))
-            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.emailText))
-            startActivity(Intent.createChooser(shareIntent,"Support"))
-        }
-        val policyButton=findViewById<TextView>(R.id.policyButton)
-        policyButton.setOnClickListener {
-            val policyIntent=Intent(Intent.ACTION_VIEW)
-            policyIntent.data=Uri.parse(getString(R.string.linkLegacy))
-            startActivity(Intent.createChooser(policyIntent,"Policy"))
+            Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.myEMail)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.emailSubject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.emailText))
+                startActivity(Intent.createChooser(this, "Support"))
+            }
+
+            val policyButton = findViewById<TextView>(R.id.policyButton)
+            policyButton.setOnClickListener {
+                val policyIntent = Intent(Intent.ACTION_VIEW)
+                policyIntent.data = Uri.parse(getString(R.string.linkLegacy))
+                startActivity(Intent.createChooser(policyIntent, "Policy"))
+            }
         }
     }
-
 }
