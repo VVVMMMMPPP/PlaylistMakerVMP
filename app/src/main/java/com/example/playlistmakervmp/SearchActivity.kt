d3100a849a8aae0d78d9e81cb0b3a6baf7a6da23
VVVMMMMPPP <Vladsmog18@mailru>
2023-05-12 12:15:20 +0300

    Sprint10try1

--- app/src/main/java/com/example/playlistmakervmp/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmakervmp/SearchActivity.kt
@@ -11,6 +11,16 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import java.util.Objects
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmakervmp.TrackAdapter
+
+data class Track(
+    val id: Long,
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val artworkUrl100: String
+)
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,42 +28,92 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
 
         val back = findViewById<ImageView>(R.id.back)
-        val clearText=findViewById<ImageView>(R.id.clear)
-        val editText=findViewById<EditText>(R.id.SearchEditText)
+        val clearText = findViewById<ImageView>(R.id.clear_text)
+        val editText = findViewById<EditText>(R.id.SearchEditText)
 
 
 
+        back.setOnClickListener {
+            back.setOnClickListener { finish() }}
 
-        back.setOnClickListener{
-            val displayIntent=Intent(this, MainActivity::class.java)
-            startActivity(displayIntent)
-        }
-
-        clearText.setOnClickListener{
-            editText.setText("")
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(editText.windowToken, 0)
-        }
-
-        editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearText.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            clearText.setOnClickListener {
+                editText.setText("")
+                val inputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(editText.windowToken, 0)
             }
-            override fun afterTextChanged(s: Editable?) {}
-        })
+        val trackAdapter = TrackAdapter(tracks)
+        val recyclerView: RecyclerView = findViewById(R.id.recycler)
+        recyclerView.adapter = trackAdapter
 
-    }
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val editText = findViewById<EditText>(R.id.SearchEditText)
-        outState.putString("myKey", editText.text.toString())
+            editText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    clearText.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+
+        }
+        override fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+            val editText = findViewById<EditText>(R.id.SearchEditText)
+            outState.putString("myKey", editText.text.toString())
+        }
+
+        override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+            super.onRestoreInstanceState(savedInstanceState)
+            val myValue = savedInstanceState.getString("myKey")
+            val editText = findViewById<EditText>(R.id.SearchEditText)
+            editText.setText(myValue)
+        }
+    private var tracks = mutableListOf<Track>()
+    init {
+
+        tracks = (1..5).map {
+            Track(
+                id = it.toLong(),
+                trackName = TRACKNAMES[it-1],
+                artistName = ARTISTS[it-1],
+                trackTime = LENGTHS[it-1],
+                artworkUrl100 = IMAGES[it-1]
+            )
+        }.toMutableList()
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val myValue = savedInstanceState.getString("myKey")
-        val editText = findViewById<EditText>(R.id.SearchEditText)
-        editText.setText(myValue)
+    companion object {
+        private val IMAGES = mutableListOf(
+            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg",
+            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg",
+            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        )
+        private val TRACKNAMES = mutableListOf(
+            "Smells Like Teen Spirit",
+            "Billie Jean",
+            "Stayin' Alive",
+            "Whole Lotta Love",
+            "Sweet Child O'Mine"
+        )
+        private val ARTISTS = mutableListOf(
+            "Nirvana",
+            "Michael Jackson",
+            "Bee Gees",
+            "Led Zeppelin",
+            "Guns N' Roses"
+        )
+        private val LENGTHS = mutableListOf(
+            "5:01", "4:35", "4:10", "5:33", "5:03"
+        )
     }
-}
+    }
+
